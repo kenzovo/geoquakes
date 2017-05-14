e604dae86603098d1edf1747a2b08b5e4ee3c94d
Kenneth Vo <kennyvo91@gmail.com>
2017-05-14 18:03:37 -0500

    added scroll bar and circle icons to scale

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,5 @@
 // define globals
 var weekly_quakes_endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
-var earthquakeIcon = {
-  url: 'images/earthquake.png',
-    // This marker is 20 pixels wide by 32 pixels high.
-    // The origin for this image is (0, 0).
-    origin: new google.maps.Point(0, 0),
-    // The anchor for this image is the base of the flagpole at (0, 32).
-    anchor: new google.maps.Point(0, 32)
-};
 
 var shape = {
    coords: [1, 1, 1, 20, 18, 20, 18, 1],
@@ -16,32 +8,34 @@ var shape = {
 
 
 $(document).ready(function() {
-  console.log("Let's get coding!");
+  console.log("Sanity Check");
 
-callAjax();
+  callAjax();
 
 }); //end documentready
 
 
 function onSubmitReqSuccess(json){
+  //  map generator
     var center ={lat: 30.2682, lng: -97.74295};
-
     var map = new google.maps.Map(document.getElementById('map'), {
       center: center,
-      zoom: 3
+      zoom: 3,
+      mapTypeId: 'terrain'
     });
 
+    console.log(json.features);
     json.features.forEach(function(earthquake){
+      // parsing for location
       var titleSplit = earthquake.properties.title.split(" ");
       var protoTitleSplit = titleSplit.slice(6, titleSplit.length);
-//*************************
+
+        //Time calculations
         var thePresent = new Date();
         var quakeTime = earthquake.properties.time;
         var days = Math.floor((thePresent-quakeTime)/(60*60*1000*24));
         var hours = Math.floor((thePresent-quakeTime)/(60*60*1000));
         var min = Math.floor((thePresent-quakeTime) /60000);
-
-
           if (hours === 1) {
             var timeHolder = ' hour ago in:'
           } else if (hours < 1){
@@ -53,19 +47,25 @@ function onSubmitReqSuccess(json){
           } else {
             timeHolder = ' hours ago in:'
           }
-
-        $('#info').append(hours + timeHolder);
-//************************
+      // append information from JSON
+      $('#info').append(hours + timeHolder);
       $("#info").append(`<p><b>${protoTitleSplit.join(" ")}</b></p>`);
       $("#info").append(`<p>${new Date(earthquake.properties.time)}</p><br></br>`);
 
       var pinlocations = {lat:earthquake.geometry.coordinates[1], lng: earthquake.geometry.coordinates[0]};
-
+      var scaleSize = Math.pow(2, earthquake.properties.mag) / 2;
       var marker = new google.maps.Marker({
           position: pinlocations,
           map: map,
-          title: 'Hello World',
-          icon: earthquakeIcon,
+          // title: protoTitleSplit,
+          icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: scaleSize,
+            fillColor: 'red',
+            fillOpacity: .2,
+            strokeColor: 'white',
+            strokeWeight: .5
+          },
           shape: shape
       });
     });
